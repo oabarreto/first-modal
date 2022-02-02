80ef300377508b18d84724d8172ccdc04d9c351b
Otávio Barreto <90889081+oabarreto@users.noreply.github.com>
2022-02-02 14:14:40 -0300

    Update, Responsividade.

--- modal.js
+++ modal.js
@@ -2,6 +2,8 @@ const openModalBtn = document.getElementById("openModal");
 
 const modalWrapper = document.querySelector(".modal-wrapper");
 
+const closeClick = document.querySelector(".click");
+
 const test = openModalBtn.addEventListener("click", function () {
   modalWrapper.classList.remove("invisible");
   isInvisibleOn = modalWrapper.classList.contains("invisible");
@@ -13,4 +15,8 @@ document.addEventListener("keydown", function (event) {
   if (keyCode) {
     modalWrapper.classList.add("invisible");
   }
+});
+
+closeClick.addEventListener("click", function () {
+  modalWrapper.classList.add("invisible");
 });
